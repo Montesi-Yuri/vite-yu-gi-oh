214f498dd669ed794c54f573e80c5d42e6abf4bf
Montesi-Yuri <130767097+Montesi-Yuri@users.noreply.github.com>
2023-07-17 13:55:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import Component1 from './components/Component1.vue'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    Component1
+  }
+}
+
+</script>
+
+<template>
+
+  <header>
+  </header>
+
+  <main>
+  </main>
+
+  <footer>
+  </footer>
+
+</template>
+
+<style lang="scss" scoped>
+@use './assets/scss/main.scss';
+
+</style>
---
+++ src/components/Component1.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+  }
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
